bd65aca26ab7dfd5325f16904b9a7de23c66d650
Rohan Rajguru <rohan.rajguru@outlook.com>
2024-09-21 17:59:29 +0530

    update scenario files

--- CKS/apiserver-troubleshoot-01/background.sh
+++ CKS/apiserver-troubleshoot-01/background.sh
@@ -3,3 +3,18 @@
 
 mkdir -p /etc/kubernetes/auditing/rules
 mkdir -p /etc/kubernetes/auditing/logs
+touch /etc/kubernetes/auditing/rules/rules.yaml
+
+
+cat   > /etc/kubernetes/auditing/rules/rules.yaml <<EOF
+
+# Log all requests at the Metadata level.
+apiVersion: audit.k8s.io/v1
+kind: Policy
+rules:
+- level: Metadata
+
+EOF
+
+
+
