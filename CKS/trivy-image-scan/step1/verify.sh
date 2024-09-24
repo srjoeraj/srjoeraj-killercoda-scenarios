fbcbfc91c0bf767d5d79e1a419dd15d8ebcf0d7c
Rohan Rajguru <rohan.rajguru@outlook.com>
2024-09-24 19:30:32 +0530

    adds verification

--- CKS/trivy-image-scan/step1/verify.sh
+++ CKS/trivy-image-scan/step1/verify.sh
@@ -1,6 +1,69 @@
-
-#!/bin/bash
+#/bin/bash
 #
-#
-# Enter some stuff!
 
+manga=$(kubectl  get deploy -n first-lab  manga-view-app  -o jsonpath={.spec.replicas})
+
+
+first=$(kubectl  get deploy -n first-lab  first-go-app  -o jsonpath={.spec.replicas})
+
+mom=$(kubectl  get deploy -n first-lab  mom-florist-app  -o jsonpath={.spec.replicas})
+
+
+site=$(kubectl  get deploy -n first-lab  personal-website  -o jsonpath={.spec.replicas})
+
+
+hmm=$(kubectl  get deploy -n first-lab  hmm  -o jsonpath={.spec.replicas})
+
+
+echo "$manga"
+
+echo "$first"
+
+echo "$mom"
+
+echo "$site"
+
+echo "$hmm"
+
+
+if [[  "$hmm" -eq 2 ]]; then
+    echo "pass"
+else
+    echo "fail"
+    exit 1
+fi
+
+
+
+if [[  "$manga" -eq 0 ]]; then
+    echo "pass"
+else
+    echo "fail"
+    exit 1
+fi
+
+
+if [[  "$first" -eq 0 ]]; then
+    echo "pass"
+else
+    echo "fail"
+    exit 1
+fi
+
+
+if [[  "$site" -eq 0 ]]; then
+    echo "pass"
+else
+    echo "fail"
+    exit 1
+fi
+
+
+
+
+if [[  "$mom" -eq 0 ]]; then
+    echo "pass"
+else
+    echo "fail"
+    exit 1
+fi
