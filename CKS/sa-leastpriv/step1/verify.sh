d322b2d8f7cc6de0788ddb0b09fd8869b5b47c99
Rohan Rajguru <rohan.rajguru@outlook.com>
2024-09-24 13:14:56 +0530

    verify1

--- CKS/sa-leastpriv/step1/verify.sh
+++ CKS/sa-leastpriv/step1/verify.sh
@@ -1,9 +1,25 @@
 #/bin/bash
 #
 
-out=$(kubectl -n moderators get secrets moderator-user-5 -o jsonpath={.data.password})
+role=$(kubectl -n chemistry get rolebinding guest-rolebinding -o jsonpath={.roleRef.name})
 
-if [[ "$out" == "TUFOR08xMjM=" ]]; then
+resource=$(kubectl -n chemistry get role "$role" -o jsonpath={.rules[*].resources[*]})
+
+
+permission=$(kubectl -n chemistry get role "$role" -o jsonpath={.rules[*].verb[*]})
+
+
+
+if [[ "$resource" == "deployments" ]]; then
+    echo "success"
+else
+    echo "Failed"
+    exit 1
+fi
+
+
+
+if [[ "$permission" == "get" ]]; then
     echo "success"
 else
     echo "Failed"
