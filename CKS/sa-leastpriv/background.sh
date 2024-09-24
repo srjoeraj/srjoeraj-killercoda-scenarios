9034ac7f1283eb4adec56f6e9d49b5654260491b
Rohan Rajguru <rohan.rajguru@outlook.com>
2024-09-24 12:10:47 +0530

    setup bg

--- CKS/sa-leastpriv/background.sh
+++ CKS/sa-leastpriv/background.sh
@@ -1,115 +1,29 @@
 #!/bin/bash 
 
+kubectl create ns chemistry
 
-kubectl apply -f - <<EOF
-
-apiVersion: v1
-kind: Namespace
-metadata:
-  creationTimestamp: null
-  name: moderators
-spec: {}
-status: {}
-
----
-
-apiVersion: v1
-data:
-  password: TFk0MjhTVU1IWQ==
-  username: c2FrdXJh
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-2
-  namespace: moderators
-
----
-
-apiVersion: v1
-data:
-  password: SEI0Mlk4QTlTSEE=
-  username: c2FzdWtl
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-1
-  namespace: moderators
+kubectl -n chemistry create sa mendeleev
+kubectl -n chemistry create sa faraday
+kubectl -n chemistry create sa fleming
+kubectl -n chemistry create sa curie
 
 
----
+kubectl -n chemistry  create role   assistant-role --verb=get,list,watch --resource=deployments,statefulsets,pvc
+kubectl -n chemistry  create role   professor-role --verb=create,delete --resource=pods,configmaps
 
 
-apiVersion: v1
-data:
-  password: TUdJV004TjQwMA==
-  username: aGluYXRh
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-3
-  namespace: moderators
+kubectl -n chemistry create rolebinding professor-rolebinding --role=professor-role --serviceaccount=chemistry:curie
 
----
+kubectl -n chemistry create rolebinding headmaster-rolebinding --clusterrole=admin --serviceaccount=chemistry:mendeleev
 
 
-apiVersion: v1
-data:
-  password: RUk0TUxWRThOWQ==
-  username: b2JpdG8=
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-4
-  namespace: moderators
-
----
-
-
-apiVersion: v1
-data:
-  password: Q0hBTkdFTUUxMjM=
-  username: bWFkYXJh
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-5
-  namespace: moderators
-
----
+kubectl -n chemistry create rolebinding guest-rolebinding --role=professor-role --serviceaccount=chemistry:faraday
 
 
 
-apiVersion: v1
-data:
-  password: N1pURzdOMkcyTw==
-  username: bWluYXRv
-kind: Secret
-metadata:
-  creationTimestamp: null
-  name: moderator-user-6
-  namespace: moderators
 
----
 
-apiVersion: v1
-kind: Pod
-metadata:
-  creationTimestamp: null
-  labels:
-    run: mod-machine
-  name: mod-machine
-  namespace: moderators
-spec:
-  volumes:
-  - name: sec-vol
-    secret:
-     secretName: moderator-user-2
-  containers:
-  - image: nginx:alpine
-    name: mod-machine
-    resources: {}
-    volumeMounts: 
-    - name: sec-vol
-      mountPath: /opt/sec
 
-EOF
+
+
+
